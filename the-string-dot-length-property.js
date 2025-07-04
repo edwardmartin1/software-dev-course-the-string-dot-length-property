613612bacc6c44b39df5a86745f299d9aa6ecef5
edwardmartin1 <edwardmartin1@yahoo.com>
2025-07-04 16:34:19 -0500

    completed code for practice using dot length

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -26,5 +26,10 @@ let lengthThree; // Length of stringThree
 
 // Your code here
 
+lengthOne = stringOne.length;
+lengthTwo = stringTwo.length;
+lengthThree = stringThree.length;
 
-
+console.log("lengthOne: " + lengthOne);
+console.log("lengthTwo: " + lengthTwo);
+console.log("lengthThree: " + lengthThree);
